715f5f105ce287ef100b5cbb9130ee325dee22dc
sachanvivek <139702466+sachanvivek@users.noreply.github.com>
2026-07-01 17:03:09 +0530

    Delete CPCM directory
    removing due to some files missing

--- CPCM/src/agent_orch/utils/testDB.py
+++
@@ -1,8 +0,0 @@
-from DBConnect import DBConnect  
-try:
-    db = DBConnect() 
-    print(f"Connected using DBConnect class")
-    db.close()
-
-except Exception as e:
-    print(f" Connection failed: {e}")
